59e98ae68eca846fa7cf79263e7c27e8d0ae2e84
Kudzai Nyandoro <kudzain@gmail.com>
2016-10-29 14:53:01 -0700

    Implement show action and refactor post controller and spec.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 100.times do |post|
-  post = Post.create(date: Date.today, rationale: "#{post} This is the rationale")
+  post = Post.create!(date: Date.today, rationale: "#{post} This is the rationale")
 end
 
 puts "100 posts have been created"
