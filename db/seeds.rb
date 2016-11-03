a1dd8e1faac2b8496d2ddb323d4689056aea2a6d
Kudzai Nyandoro <kudzain@gmail.com>
2016-11-03 05:59:06 -0700

    Update the post_dashboard.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 
-@user = User.create!(first_name: "User", last_name: "Example", email: "user@example.com",  password: "foobar", password_confirmation: "foobar")
+@user = User.create!(first_name: "User", last_name: "Example", email: "user@example.com",  password: "passpass", password_confirmation: "passpass")
+AdminUser.create!(first_name: "Admin", last_name: "User", email: "admin@example.com",  password: "passpass", password_confirmation: "passpass")
+
 puts "*******************************"
 
-puts "One user was created"
+puts "User and AdminUser were created"
 
 100.times do |post|
   post = Post.create!(date: Date.today, rationale: "#{post} This is the rationale", user_id: @user.id)
